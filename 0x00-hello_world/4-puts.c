f538a4560f01c01ffc30659ad9c419d1066e0635
Ogbonna Ikenna <cindionline@yahoo.com>
2022-03-11 07:58:27 -0800

    C Programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- *  main - prints the phrase "Programming is like building a multilingual puzzle"
+ *  main - prints a phrase
  *
  *  Return: 0 after printing the function
  */
