e6e34466317283c94953540271009acfda08af3e
Ogbonna Ikenna <cindionline@yahoo.com>
2022-03-11 06:51:42 -0800

    C programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 # include <stdio.h>
-int main(void){
-puts("Programming is like building a multilingual puzzle");
-return (0);
+/*
+ *  prints the phrase "Programming is like building a multilingual puzzle"
+ *
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
