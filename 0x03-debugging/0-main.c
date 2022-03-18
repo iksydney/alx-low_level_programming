025090817fa46a61c008ab5d78ac5ae290c88dd2
Ogbonna Ikenna <cindionline@yahoo.com>
2022-03-18 04:05:09 +0100

    tests function that prints if integer is positive or negative

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* main - negative or positive
+* Description: tests function that prints if integer is positive or
+* negative
+* Return: 0;
+*/
+
+int main(void)
+{
+	int i = 0;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
