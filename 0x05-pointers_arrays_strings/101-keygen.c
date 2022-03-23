10255d3d594dea456660aa2b2544c2956b22e638
Ogbonna Ikenna <cindionline@yahoo.com>
2022-03-23 04:11:34 +0100

    advanced

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,39 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <math.h>
 
 /**
- * main - print password.
+ * main - random password generator for 101-crackme
  *
- * Return: 0.
+ * Return: always 0
  */
-
 int main(void)
 {
-	int ascii = 2772, i = 0, j, random;
-	char password[100];
-	time_t t;
+	int i, j, k, s;
+	char c[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char p[58];
 
-	srand((int) time(&t));
-	while (ascii > 126)
+	srand(time(NULL));
+	while (s != 2772)
 	{
-		random = rand() % 126;
-		password[i] = random;
-		ascii -= random;
-		i++;
+		i = k = s = 0;
+		while ((2772 - 122) > s)
+		{
+			j = rand() % 62;
+			p[i] = c[j];
+			s += c[j];
+			i++;
+		}
+		while (c[k])
+		{
+			if (c[k] == (2772 - s))
+			{
+				p[i] = c[k];
+				s += c[k];
+				i++;
+				break;
+			}
+			k++;
+		}
 	}
-	if (ascii > 0)
-		password[i] = ascii;
-	else
-	{
-		i--;
-	}
-	
-
-	for (j = 0; j <= i; j++)
-	{
-		printf("%c", password[j]);
-	}
+	p[i] = '\0';
+	printf("%s", p);
 	return (0);
 }
