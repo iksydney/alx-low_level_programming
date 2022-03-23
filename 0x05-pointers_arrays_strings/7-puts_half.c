2062e86fdfe1b57d2bbbe1578a4aea3195f7bd04
Ogbonna Ikenna <cindionline@yahoo.com>
2022-03-23 04:01:02 +0100

    function that prints strings of characters in half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,10 +1,10 @@
 #include "main.h"
-/**
-* puts_half - a function that prints half of a string, followed by a new line
-* @str: An input string
-* Return: Nothing
-*/
 
+/**
+ * puts_half - a function that prints half of a string, followed by a new line.
+ * @str: An input string
+ * Return: Nothing
+ */
 void puts_half(char *str)
 {
 	int len = 0, i, n;
@@ -19,7 +19,7 @@ void puts_half(char *str)
 		n = (len + 1) / 2;
 
 	for (i = n; i < len; i++)
-		_putchar(str[i];
+		_putchar(str[i]);
 
 	_putchar('\n');
 }
