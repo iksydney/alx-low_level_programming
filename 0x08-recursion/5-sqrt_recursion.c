94dc2e25ff3e4a4e1ca7fcaa31738b3122c84fd4
iksydney <cindionline@yahoo.com>
2022-04-01 12:23:42 +0100

    recursive function

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * _sqrt2 - returns the natural square root of a number
- * @a - integer
- * @b - integer that iterates from 1 to n
+ * sqrt2 - returns the natural square root of a number
+ * @a: integer
+ * @b: integer that iterates from 1 to n
  * Return: success
  * on error: -1 is returned
  */
@@ -13,4 +13,16 @@ int sqrt2(int a, int b)
 	else if (b * b > a)
 		return (-1);
 	return (sqrt2(a, b + 1));
+}
+
+/**
+ * _sqrt_recursion - returns the natural square root of n
+ * @n: Number Integer
+ *
+ * Return: on success 1.
+ * On error, -1 is returned
+ */
+int _sqrt_recursion(int n)
+{
+	return (sqrt2(n, 1));
 }
