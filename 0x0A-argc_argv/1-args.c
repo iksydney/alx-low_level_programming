0fa337549a27f8d70aa70b23c71390d98c61da78
iksydney <cindionline@yahoo.com>
2022-04-07 19:37:47 +0100

    Prints the number of arguments passed to it

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * @argc: This counts the number of parameters that go into main
+ * @argv: pointer of array of pointer containing strings entering main
+ * Return: Always 0 for success
+ */
+int main(int argc, char **argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
