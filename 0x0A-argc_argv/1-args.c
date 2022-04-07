06e574593a480f1102315d2844296ea1a5414b43
iksydney <cindionline@yahoo.com>
2022-04-07 19:40:49 +0100

    Prints the number of arguments passed to it

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -8,9 +8,8 @@
  * @argv: pointer of array of pointer containing strings entering main
  * Return: Always 0 for success
  */
-int main(int argc, char **argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-	(void) argv;
-	printf("%i\n", argc - 1);
+	printf("%d\n", argc - 1);
 	return (0);
 }
